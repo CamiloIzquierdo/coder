2cfe41406847a47cba96468bce8543f71eafb121
CamiloIzquierdo <camiloi.99@hotmail.com>
2023-08-09 11:09:35 -0300

    cambio de color navbar

--- src/components/Navbar/NavbarData.jsx
+++ src/components/Navbar/NavbarData.jsx
@@ -23,7 +23,7 @@ export const NavbarData = ({
                 }
             >
                 <li className="w-full flex flex-col items-center justify-center gap-1">
-                    <span className="block py-2 pl-3 pr-4 text-slate-800 rounded xl:font-semibold justify-start lg:hover:bg-transparent lg:border-0 lg:hover:text-primary lg:p-0 ">
+                    <span className="block py-2 pl-3 pr-4 text-tertiary rounded xl:font-semibold justify-start lg:hover:bg-transparent lg:border-0 lg:hover:text-primary lg:p-0 ">
                         Servicios
                     </span>
                     <div className="h-[2px] w-9 bg-primary justify-center items-center hidden xl:flex"></div>
@@ -43,7 +43,7 @@ export const NavbarData = ({
                 }
             >
                 <li className="w-full flex flex-col items-center justify-center gap-1">
-                    <span className="block py-2 pl-3 pr-4 text-slate-800 rounded xl:font-semibold justify-start lg:hover:bg-transparent lg:border-0 lg:hover:text-primary lg:p-0 ">
+                    <span className="block py-2 pl-3 pr-4 text-tertiary rounded xl:font-semibold justify-start lg:hover:bg-transparent lg:border-0 lg:hover:text-primary lg:p-0 ">
                         Sobre mi
                     </span>
                     <div className="h-[2px] w-9 bg-primary justify-center items-center hidden xl:flex"></div>
@@ -63,7 +63,7 @@ export const NavbarData = ({
                 }
             >
                 <li className="w-full flex flex-col items-center justify-center gap-1">
-                    <span className="block py-2 pl-3 pr-4 text-slate-800 rounded xl:font-semibold justify-start lg:hover:bg-transparent lg:border-0 lg:hover:text-primary lg:p-0 ">
+                    <span className="block py-2 pl-3 pr-4 text-tertiary rounded xl:font-semibold justify-start lg:hover:bg-transparent lg:border-0 lg:hover:text-primary lg:p-0 ">
                         Testimonios
                     </span>
                     <div className="h-[2px] w-9 bg-primary justify-center items-center hidden xl:flex"></div>
@@ -83,7 +83,7 @@ export const NavbarData = ({
                 }
             >
                 <li className="px-3 xl:px-0">
-                    <span className="block py-2 px-10 text-white xl:font-semibold lg:border-0 lg:hover:text-slate-800 lg:py-3 lg:px-10 bg-primary xl:rounded-3xl">
+                    <span className="block py-2 px-10 text-white xl:font-semibold lg:border-0 lg:hover:text-tertiary lg:py-3 lg:px-10 bg-primary xl:rounded-3xl">
                         Contacto
                     </span>
                 </li>
