34052ab3a411917d1f9bc2c088cce0f54b75c239
CamiloIzquierdo <camiloi.99@hotmail.com>
2023-08-13 18:37:20 -0300

    fix delay

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -20,27 +20,36 @@ export const Header = () => {
                 className="h-full w-full bg-center bg-cover"
             >
                 <div className="bg-primary h-full flex items-center xl:px-32 px-7 w-full bg-opacity-70">
-                    <div
-                        className={`${
-                            isVisible
-                                ? "ease-in opacity-100 transform transition-all duration-1000 delay-500 flex flex-col gap-12 translate-y-0"
-                                : "opacity-0 transform transition-all duration-1000 delay-500 translate-y-full"
-                        }`}
-                    >
-                        <span className="text-white flex flex-col text-sm xl:text-3xl new-hero-ultra-italic gap-2 ">
-                            <span>
-                                “El que se conoce a sí mismo es iluminado
+                    <div className="flex flex-col gap-12">
+                        <div
+                            className={`${
+                                isVisible
+                                    ? "ease-in opacity-100 transform transition-all duration-1000 delay-500 translate-y-0"
+                                    : "opacity-0 transform transition-all duration-1000 delay-500 translate-y-full"
+                            }`}
+                        >
+                            <span className="text-white flex flex-col text-sm xl:text-3xl new-hero-ultra-italic gap-2 ">
+                                <span>
+                                    “El que se conoce a sí mismo es iluminado
+                                </span>
+                                <span>
+                                    El que se domina a sí mismo es poderoso{" "}
+                                    <br />
+                                </span>
+                                <span>
+                                    El que se conquista a sí mismo es{" "}
+                                    <strong>INVENCIBLE</strong>”
+                                </span>
+                                <span> Lao-Tse</span>
                             </span>
-                            <span>
-                                El que se domina a sí mismo es poderoso <br />
-                            </span>
-                            <span>
-                                El que se conquista a sí mismo es{" "}
-                                <strong>INVENCIBLE</strong>”
-                            </span>
-                            <span> Lao-Tse</span>
-                        </span>
-                        <div className="flex flex-col h-full xl:gap-5 gap-1">
+                        </div>
+                        <div
+                            className={`${
+                                isVisible
+                                    ? "ease-in opacity-100 transform transition-all duration-1000 delay-[1500ms] translate-y-0 flex flex-col h-full xl:gap-5 gap-1"
+                                    : "opacity-0 transform transition-all duration-1000 delay-500 translate-y-full"
+                            }`}
+                        >
                             <span className="xl:text-8xl text-5xl new-hero-ultra text-white">
                                 Manuel Lucero
                             </span>
